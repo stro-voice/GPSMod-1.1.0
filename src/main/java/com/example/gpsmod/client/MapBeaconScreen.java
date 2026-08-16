9eb3e0be94a19e7be8a136f3d4e131d4e50377cc
stro-voice <stro230411@gmail.com>
2026-08-16 14:15:44 +0400

    1.3.1

--- src/main/java/com/example/gpsmod/client/MapBeaconScreen.java
+++ src/main/java/com/example/gpsmod/client/MapBeaconScreen.java
@@ -1,52 +1,43 @@
 package com.example.gpsmod.client;
 
-import com.example.gpsmod.network.C2SSelectBeaconPacket;
-import com.example.gpsmod.network.PacketHandler;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.widget.button.Button;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.StringTextComponent;
 
-import java.util.List;
-
 public class MapBeaconScreen extends Screen {
-    private final List<BlockPos> beacons;
 
-    public MapBeaconScreen(List<BlockPos> beacons) {
-        super(new StringTextComponent("GPS Маяки"));
-        this.beacons = beacons;
+    // ПРАВИЛЬНЫЙ КОНСТРУКТОР (без параметров)
+    public MapBeaconScreen() {
+        super(new StringTextComponent("GPS Меню"));
     }
 
     @Override
     protected void init() {
-        int cx = this.width / 2;
-        int cy = this.height / 2 - 50;
-
-        int i = 0;
-        for (BlockPos pos : beacons) {
-            if (i >= 5) break;
-            this.addButton(new Button(cx - 90, cy + (i * 22), 180, 20,
-                    new StringTextComponent("🚩 Маяк [" + pos.getX() + ", " + pos.getZ() + "]"),
-                    b -> {
-                        PacketHandler.CHANNEL.sendToServer(new C2SSelectBeaconPacket(pos));
-                        this.onClose();
-                    }));
-            i++;
-        }
-
-        this.addButton(new Button(cx - 90, cy + (i * 22) + 10, 180, 20,
-                new StringTextComponent("❌ Сбросить цель"),
-                b -> {
-                    PacketHandler.CHANNEL.sendToServer(new C2SSelectBeaconPacket(null));
-                    this.onClose();
-                }));
+        super.init();
+        
+        // Кнопка сброса маршрута
+        this.addButton(new net.minecraft.client.gui.widget.button.Button(
+            this.width / 2 - 60, this.height / 2 + 40, 120, 20,
+            new StringTextComponent("Сбросить маршрут"),
+            (button) -> {
+                // Отправляем null для сброса активной цели
+                com.example.gpsmod.network.PacketHandler.sendToServer(
+                    new com.example.gpsmod.network.C2SSelectBeaconPacket(null)
+                );
+                this.onClose();
+            }
+        ));
     }
 
     @Override
-    public void render(MatrixStack ms, int mx, int my, float pt) {
-        this.renderBackground(ms);
-        drawCenteredString(ms, this.font, "Выберите Флаг-Цель:", this.width / 2, this.height / 2 - 70, 0xFFFFFF);
-        super.render(ms, mx, my, pt);
+    public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+        this.renderBackground(matrixStack);
+        drawCenteredString(matrixStack, this.font, "Выбор точки навигации", this.width / 2, 20, 0xFFFFFFFF);
+        super.render(matrixStack, mouseX, mouseY, partialTicks);
+    }
+    
+    @Override
+    public boolean isPauseScreen() {
+        return false;
     }
 }
